8c1f50f542e831d38b62b9e5fda0e067ca28905b
Getteli <38441697+Getteli@users.noreply.github.com>
2022-11-05 15:52:05 -0300

    Add files via upload

---
+++ script_form_to_sheet_googledrive.js
@@ -0,0 +1,50 @@
+var sheetName = 'Leads Anunciantes';
+var scriptProp = PropertiesService.getScriptProperties();
+
+const intialSetup = (e = {}) =>
+{
+	var activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+	scriptProp.setProperty('key', activeSpreadsheet.getId())
+}
+
+const doPost = (e = {}) =>
+{
+	const { parameter, postData: { contents, type } = {} } = e;
+	const { source } = parameter;
+	var lock = LockService.getScriptLock()
+	lock.tryLock(10000)
+
+	try
+	{
+		// esse id é o id da planilha (abra a planilha em uma aba ao lado e veja o id na url)
+		var sheet = SpreadsheetApp.openById("1_GIDaX6VFDtRXnj4KdH457d9UtvtP8rhU_ZJtUeROJU").getActiveSheet();
+		var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
+		var nextRow = sheet.getLastRow() + 1
+
+		const retailer = JSON.parse(contents);
+
+		var newRow = sheet.appendRow([
+			new Date(),
+			retailer.email,
+			retailer.primeiro_nome,
+			retailer.ultimo_nome,
+			retailer.nome_empresa,
+			retailer.cargo,
+			retailer.telefone,
+		])
+	
+		return ContentService
+			.createTextOutput(JSON.stringify({ 'result': 'success'}))
+			.setMimeType(ContentService.MimeType.JSON)
+	}
+	catch (e)
+	{
+		return ContentService
+			.createTextOutput(JSON.stringify({ 'result': 'error', 'error': e.message }))
+			.setMimeType(ContentService.MimeType.JSON)
+	}
+	finally
+	{
+		lock.releaseLock()
+	}
+}
